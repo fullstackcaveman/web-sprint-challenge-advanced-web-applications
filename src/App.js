37cffb2d52141c91e5411f42207281ae945acb91
Chris Burton <chris@fullstackcaveman.com>
2021-03-26 10:18:03 -0600

    Private route to BubblesPage working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
 import Login from './components/Login';
 import './styles.scss';
+import BubblePage from './components/BubblePage';
 
 const initialFormValues = {
 	username: '',
@@ -22,6 +24,10 @@ function App() {
 				<Route exact path='/' component={Login}>
 					<Login values={formValues} change={inputChange} />
 				</Route>
+				<Route exact path='/login' component={Login}>
+					<Login values={formValues} change={inputChange} />
+				</Route>
+				<PrivateRoute exact path='/bubble-page' component={BubblePage} />
 			</div>
 		</Router>
 	);
